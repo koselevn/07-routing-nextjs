c6a8e027e715d22f1e3697c2f2e96fb5ed9db733
koselevn <koselevn52@gmail.com>
2025-11-04 23:14:17 +0100

    v-2

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,7 +2,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import * as Yup from "yup";
 import css from "./NoteForm.module.css";
-import type { FormValues } from "../../../types/note";
+import type { FormValues } from "../../types/note";
 import { createNote } from "../../lib/api";
 
 interface NoteFormProps {
